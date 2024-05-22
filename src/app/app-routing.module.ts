556c675dde479628da11150981c98f3c6b92313a
anisberto dos reis do nascimento <anisbertoos@gmail.com>
2024-05-21 22:18:22 -0300

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './paginas/home/home.component';
+import { CadastroComponent } from './componentes/quadras/cadastro/cadastro.component';
+import { ListagemComponent } from './componentes/quadras/listagem/listagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'listagem', component: ListagemComponent },
+  { path: '**', redirectTo: 'Home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './componentes/quadras/cadastro/cadastro.component';
+import { ListagemComponent } from './componentes/quadras/listagem/listagem.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    ListagemComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
